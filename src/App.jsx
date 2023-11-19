1e66b23fbfcd98f23d17e2296a14aede5ddaa0e7
somunachi <109367682+somunachi@users.noreply.github.com>
2023-11-19 15:00:02 +0100

    resolving error

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import Aboutme from './components/Aboutme';
 import Skillset from './components/Skillset';
 import Projects from './components/Projects';
 import Connect from './components/Connect';
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 
 function App() {
   return (
-  <body>
+  <div id='body'>
     <Hero />
     <Aboutme />
     <Skillset />
     <Projects />
     <Connect />
-  </body>
+  </div>
   )
 }
 
